dec216beaa6bc15819f416d7e9dff84f5822f661
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2020-04-07 10:04:40 +0200

    download data to be able to run notebook doc

---
+++ download_sam_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd share/data/microscopy
+mkdir "20170817 MS-E27 LD qDII-CLV3-DR5"
+cd "20170817 MS-E27 LD qDII-CLV3-DR5"
+mkdir RAW
+cd RAW
+wget http://flower.ens-lyon.fr/sam_patterning/data/qDII-CLV3-DR5-E27-LD-SAM7.czi
+wget http://flower.ens-lyon.fr/sam_patterning/data/qDII-CLV3-DR5-E27-LD-SAM7-T5.czi
+wget http://flower.ens-lyon.fr/sam_patterning/data/qDII-CLV3-DR5-E27-LD-SAM7-T10.czi
+cd ..
+mkdir TIF-No-organs
+cd TIF-No-organs
+wget http://flower.ens-lyon.fr/sam_patterning/data/qDII-CLV3-DR5-E27-LD-SAM7-No-organs.tif
+wget http://flower.ens-lyon.fr/sam_patterning/data/qDII-CLV3-DR5-E27-LD-SAM7-T5-No-organs.tif
+wget http://flower.ens-lyon.fr/sam_patterning/data/qDII-CLV3-DR5-E27-LD-SAM7-T10-No-organs.tif
+cd ../..
+cd ../../..
